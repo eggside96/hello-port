f9217061e50290a4255d3f2ab13a7eeb44e5195a
Akies01 <113812378+Akies01@users.noreply.github.com>
2024-04-18 13:58:53 +0900

    update navbar-controll.js

--- navbar-controll.js
+++ navbar-controll.js
@@ -4,9 +4,17 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+    var aboutPage = document.getElementById("about-page");
+
+    // 'about-page'가 존재하면 네비게이션 바 숨기기
+    if (aboutPage) {
+        navbar.style.display = 'none';
     } else {
-        navbar.classList.remove("sticky");
+        // 기존의 sticky navbar 로직
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
     }
 }
