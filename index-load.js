d95a75e758cdc809abda3d9afd9a8193a020b65b
Akies01 <113812378+Akies01@users.noreply.github.com>
2024-04-18 13:17:46 +0900

    update index-load.js

--- index-load.js
+++ index-load.js
@@ -1,30 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // 페이지 섹션 별로 HTML 로드
+    loadHtml('main-page', 'main-page.html');
+    loadHtml('nav-page', 'nav-page.html');
+    loadHtml('about-page', 'about-page.html');
+    loadHtml('project-page', 'project-page.html');
 
-    // 어두운 섹션 로드
-    fetch('main-page.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('main-page').innerHTML = html;
-        });
-
-    // 내비바 로드
-    fetch('nav-page.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('nav-page').innerHTML = html;
-        });
-
-    // 밝은 섹션 로드
-    fetch('about-page.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('about-page').innerHTML = html;
-        });
-
-    // 프로젝트 섹션 로드
-    fetch('project-page.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('project-page').innerHTML = html;
-        });
+    // HTML을 로드하고 DOM에 삽입하는 함수
+    function loadHtml(elementId, url) {
+        fetch(url)
+            .then(response => response.text())
+            .then(html => {
+                document.getElementById(elementId).innerHTML = html;
+            })
+            .catch(error => console.error(`Error loading the ${elementId}:`, error));
+    }
 });
